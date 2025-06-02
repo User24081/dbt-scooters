ba90fcec2b38bb233462e17a3739dc16ffbe5912
User24081 <100943015+User24081@users.noreply.github.com>
2025-06-02 12:21:04 +0300

    end of mission 8.1

--- models/revenue_daily.sql
+++ models/revenue_daily.sql
@@ -1,7 +1,7 @@
 select
     sum(price_rub) as revenue_rub,
     "date",
-    now() at time zone 'utc' as updated_at
+    {{ updated_at() }}
 from
     {{ ref("trips_prep") }}
 {% if is_incremental() %}
@@ -9,6 +9,7 @@ where
     "date" >= (select max("date") - interval '2' day from {{ this }})
 {% endif %}
 group by
-    2
+    2,
+    3
 order by
     2
