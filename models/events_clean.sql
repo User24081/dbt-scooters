b3c0bcaebbc5838c9c2094407dd034d9bc754c09
User24081 <100943015+User24081@users.noreply.github.com>
2025-06-02 12:45:22 +0300

    end of mission 8.4

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,13 +1,18 @@
+{% set date = var("date", none) %}
 select distinct
     user_id,
     "timestamp",
     type_id,
-     {{ updated_at() }}
+    {{ updated_at() }}
 from
     {{ source("scooters_raw", "events") }}
 where
 {% if is_incremental() %}
-    "timestamp" > (select max("timestamp") from {{ this }})
+    {% if date %}
+        date("timestamp") = date '{{ date }}'
+    {% else %}
+        "timestamp" > (select max("timestamp") from {{ this }})
+    {% endif %}
 {% else %}
     "timestamp" < timestamp '2023-08-01'
 {% endif %}
